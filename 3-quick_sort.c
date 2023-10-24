cda2184fd57348eb5f213259298f7d0038d39f47
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-24 19:37:49 +0100

    quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,10 +27,10 @@ int partition(int *array, int low, int high, size_t size)
 			array[j] = temp;
 		}
 	}
-	print_array(array, size);
 	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
+	print_array(array, size);
 	return (i + 1);
 }
 
