14f14aef09ff49ebfe7411a26d2199fd3c421d44
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-24 18:37:41 +0100

    insertion_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *node, *temp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 	current = (*list)->next;
 	while (current)
 	{
